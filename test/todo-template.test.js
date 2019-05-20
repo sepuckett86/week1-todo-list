8a7441df5e77b6b0a36f3389f53ea183c3d007ce
sepuckett86 <sepuckett86@gmail.com>
2019-05-20 16:51:12 -0700

    added test for unchecked and adjusted function

--- test/todo-template.test.js
+++ test/todo-template.test.js
@@ -1,18 +1,23 @@
 const test = QUnit.test;
 
 function todoTemplate(todo) {
+    let checked = '';
+
+    if(todo.completed) {
+        checked = ' ' + 'checked';
+    }
     const html = /*html*/`
         <li>
             <label>
                 Go for a walk in the forest
-                <input type="checkbox" name="todo" checked>
+                <input type="checkbox" name="todo"${checked}>
             </label>
         </li>
     `;
     return html;
 }
 
-test('makes html for one to do with checked', assert => {
+test('makes html for to-do with checked', assert => {
     // Arrange
     const todo = {
         task: 'Go for a walk in the forest',
@@ -23,6 +28,26 @@ test('makes html for one to do with checked', assert => {
             <label>
                 Go for a walk in the forest
                 <input type="checkbox" name="todo" checked>
+            </label>
+        </li>
+    `;
+    // Act
+    const html = todoTemplate(todo);
+    // Assert
+    assert.htmlEqual(html, expected);
+});
+
+test('makes html for to-do without checked', assert => {
+    // Arrange
+    const todo = {
+        task: 'Go for a walk in the forest',
+        completed: false
+    };
+    const expected = /*html*/`
+        <li>
+            <label>
+                Go for a walk in the forest
+                <input type="checkbox" name="todo">
             </label>
         </li>
     `;
