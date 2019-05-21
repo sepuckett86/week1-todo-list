54d4009b3b6e4e82510020292abc488955f1e348
sepuckett86 <sepuckett86@gmail.com>
2019-05-20 17:20:14 -0700

    add headerTemplate function

---
+++ src/header-template.js
@@ -0,0 +1,7 @@
+function headerTemplate() {
+    return `
+        <h1>To Do List</h1>
+    `;
+}
+
+export default headerTemplate;
--- test/tests.js
+++ test/tests.js
@@ -4,3 +4,4 @@ import './html-equal.js';
 // tests
 import './html-to-DOM.test.js';
 import './todo-template.test.js';
+import './header-template.test.js';
